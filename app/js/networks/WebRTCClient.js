0a88258b70aa622237671bdc577a104a36e4ea72
Unboring SL <arturo@unboring.net>
2017-06-12 16:08:23 +0200

    Add new urls to ICE servers

--- app/js/networks/WebRTCClient.js
+++ app/js/networks/WebRTCClient.js
@@ -195,7 +195,11 @@
 	// ice servers for RTCPeerConnection.
 
 	var ICE_SERVERS = [
-		{ urls: 'stun:stun.l.google.com:19302 ' }
+		{ urls: 'stun:stun.l.google.com:19302' },
+		{ urls: 'stun:stun1.l.google.com:19302' },
+		{ urls: 'stun:stun2.l.google.com:19302' },
+		{ urls: 'stun:stun3.l.google.com:19302' },
+		{ urls: 'stun:stun4.l.google.com:19302' }
 	];
 
 	/**
